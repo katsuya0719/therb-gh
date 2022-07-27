bb1436dc1be2c5287ef47f51167eef4cca2c3008
K. SAITO <80165008+Saito40@users.noreply.github.com>
2022-07-28 01:48:36 +0900

    UnitTestのテスト

--- THREB-GH-Test/UnitTest1.cs
+++ THREB-GH-Test/UnitTest1.cs
@@ -1,6 +1,16 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Utils;
+using THERBgh;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Rhino.Geometry.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using Rhino.Geometry.Intersect;
+using Newtonsoft.Json;
+using Model;
 
 namespace THREB_GH_Test
 {
@@ -11,6 +21,39 @@ namespace THREB_GH_Test
         public void UtilsTest()
         {
             Assert.AreEqual(" 3", Converter.FillEmpty(3, 2));
+
+        }
+        [TestMethod]
+        public void ExBoxTest()
+        {
+            var boxes = new List<Box>();
+            boxes.Add(new Box(
+                basePlane: Plane.WorldXY,
+                xSize: new Interval(0, 4000),
+                ySize: new Interval(0, 8000),
+                zSize: new Interval(0, 8000)));
+            boxes.Add(new Box(
+                basePlane: Plane.WorldXY,
+                xSize: new Interval(4000, 8000),
+                ySize: new Interval(0, 8000),
+                zSize: new Interval(0, 3000)));
+            boxes.Add(new Box(
+                basePlane: Plane.WorldXY,
+                xSize: new Interval(4000, 8000),
+                ySize: new Interval(0, 4000),
+                zSize: new Interval(3000, 6000)));
+            boxes.Add(new Box(
+                basePlane: Plane.WorldXY,
+                xSize: new Interval(4000, 8000),
+                ySize: new Interval(4000, 8000),
+                zSize: new Interval(3000, 6000)));
+
+            var exBoxes = ExBox.SplitGeometry(boxes, 0.1);
+            foreach(var exBox in exBoxes)
+            {
+                Debug.WriteLine(exBox.BoxSurfaces.Count);
+            }
+
 
         }
     }
