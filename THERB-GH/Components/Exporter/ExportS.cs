e15d1888f3fbe529e7ef15d500a705c14d544e77
katsuya0719 <katsuya0719@gmail.com>
2022-09-08 07:57:26 +0900

    とりあえず、s.datの雛形はできた

---
+++ THERB-GH/Components/Exporter/ExportS.cs
@@ -0,0 +1,87 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Rhino.Geometry.Collections;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Rhino.Geometry.Intersect;
+using Newtonsoft.Json;
+using Model;
+using Utils;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace THERBgh
+{
+    public class ExportS : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public ExportS()
+          : base("exportS", "exportS",
+              "export s.dat",
+              "THERB-GH", "IO")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            //pManager.AddGenericParameter("Therb", "therb", "THERB class", GH_ParamAccess.item);
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("s_dat", "s_dat", "s.dat file", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            DA.SetData("s_dat", CreateDatData.CreateSDat());
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("e9758d8e-a038-4931-8eb1-86b567f982dc"); }
+        }
+    }
+}
